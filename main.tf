84a60ef26c6da1ef47566fd7970f485436af4f97
Frank <frank.migliorino@outlook.com>
2019-10-28 11:38:19 -0400

    configure load balancer

--- main.tf
+++ main.tf
@@ -5,7 +5,15 @@ provider "aws" {
 
 module "ec2" {
   source = "module/"
-  # ...
-  # ...
-  # ...
+  ami= "ami-07c307e59cd2f19ec"
+  instance_type = "t3.medium"
+  key_name= "frank"
+  subnet_id= "subnet-0271f8735ed0d9284"
+  vpc_security_group_ids = ["sg-02073367768e306bb"]
+  candidate = "frank" #your name
+
+  tags = {
+      Name = "case-study-test"
+  }
+
 }
--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,6 @@ output "public_ip" {
   value = "${module.ec2.public_ip}"
 }
 
-# ...
+output "dns_name" {
+  value = "${module.ec2.dns_name}"
+}
